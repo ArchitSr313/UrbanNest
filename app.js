414f1a660562ce2d9788158218d3d18f820848d6
ArchitSr313 <architsr313@gmail.com>
2025-01-20 21:51:48 +0530

    added seprate folder for routes

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
-const Listing=require("./models/listing.js");
 const path=require("path");
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");            //this package allows us to use common code in different pages
 const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
-const {listingSchema, reviewSchema}=require("./schema.js");
-const Review=require("./models/reviews.js");
+const listings=require("./routes/listings.js");   //listings routes
+const reviews = require("./routes/reviews.js");   //reviews routes
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -30,140 +29,14 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
-//validate schema using joi
-//implementing it using a seprate function instead of write it in every route we pass this function as middleware 
-// where schema validation is needed this function is passed as middleware
-const validateListing=(req,res,next)=>{
-    let {error}=listingSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map((el)=> el.message).join(",");
-        // throw new ExpressError(400,error);
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
-    }
-};
-
-const validateReview=(req,res,next)=>{
-    let {error}=reviewSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map((el)=> el.message).join(",");
-        // throw new ExpressError(400,error);
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
-    }
-};
 
 app.get("/",(req,res)=>{
     res.send("work in progress");
 });
 
-//index route
-app.get("/listings",wrapAsync(async (req,res)=>{
-    const allListings=await Listing.find({});
-    res.render("listings/index.ejs",{allListings});
-}));
-//new route
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs");
-});
 
-//show route
-app.get("/listings/:id",wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    const listing=await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs",{listing});
-}));
-//create route
-app.post("/listings",validateListing,wrapAsync(async (req,res,next)=>{
-    // let {title,description,image,price,location,country}=req.body;
-    // let listing=req.body;
-    // console.log(listing);
-        // if(!req.body.listing){
-        //     // throw new ExpressError(400,"Invalid Data");
-        //     next(new ExpressError(400,"Send Valid Data"));
-        // }
-        // let result=listingSchema.validate(req.body);          //using joi to handle schema validation
-        // console.log(result);
-        // if(result.error){
-        //     throw new ExpressError(400, result.error);
-        // }
-        const newListing=new Listing(req.body.listing);          //req.body.listing returns a javascript object
-
-        //it is validation for each values but it is lengthy process so we use joi
-        // if(!newListing.title){
-        //     throw new ExpressError(400,"Title not defined");
-        // }
-        // if(!newListing.description){
-        //     throw new ExpressError(400,"Description not defined");
-        // }
-        // if(!newListing.location){
-        //     throw new ExpressError(400,"Location is empty");
-        // }
-        await newListing.save();
-        res.redirect("/listings");
-}));
-
-//edit route
-app.get("/listings/:id/edit", wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    const listing=await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
-}));
-//update route
-app.put("/listings/:id",validateListing, wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    // if(!req.body.listing){
-    //     throw new ExpressError(400,"Invalid Data");
-    // }
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect(`/listings/${id}`);
-}));
-//delete route
-app.delete("/listings/:id",wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    let deletedListing= await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}));
-
-
-//Reviews route
-//Post Review Route
-app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req,res)=>{
-    let listing= await Listing.findById(req.params.id);
-    let newReview= new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    res.redirect(`/listings/${listing._id}`);
-}));
-
-//Delete Review Route
-app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res)=>{
-    let{id, reviewId} = req.params;
-    await Listing.findByIdAndUpdate(id , {$pull : {reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-
-    res.redirect(`/listings/${id}`);
-}));
-
-// app.get("/testListing",async (req,res)=>{
-//     let sampleListing=new Listing({
-//         title:"My New Villa",
-//         description:"By The Beach",
-//         price:2300,
-//         location:"Noida, UP",
-//         country:"India"
-//     });
-//     await sampleListing.save();
-//     console.log("sample was saved");
-//     res.send("successful listing");
-// });
+app.use("/listings",listings);
+app.use("/listings/:id/reviews", reviews);
 
 
 //middle ware for handling backend errors
---
+++ routes/listings.js
@@ -0,0 +1,107 @@
+const express=require("express");
+const router=express.Router();
+const wrapAsync=require("../utils/wrapAsync.js");
+const Listing=require("../models/listing.js");
+const {listingSchema}=require("../schema.js");
+
+//validate schema using joi
+//implementing it using a seprate function instead of write it in every route we pass this function as middleware 
+// where schema validation is needed this function is passed as middleware
+const validateListing=(req,res,next)=>{
+    let {error}=listingSchema.validate(req.body);
+    if(error){
+        let errMsg=error.details.map((el)=> el.message).join(",");
+        // throw new ExpressError(400,error);
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+};
+
+//index route
+router.get("/",wrapAsync(async (req,res)=>{
+    const allListings=await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
+}));
+
+//new route
+router.get("/new",(req,res)=>{
+    res.render("listings/new.ejs");
+});
+
+//show route
+router.get("/:id",wrapAsync(async (req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id).populate("reviews");
+    res.render("listings/show.ejs",{listing});
+}));
+
+//create route
+router.post("/",validateListing,wrapAsync(async (req,res,next)=>{
+    // let {title,description,image,price,location,country}=req.body;
+    // let listing=req.body;
+    // console.log(listing);
+        // if(!req.body.listing){
+        //     // throw new ExpressError(400,"Invalid Data");
+        //     next(new ExpressError(400,"Send Valid Data"));
+        // }
+        // let result=listingSchema.validate(req.body);          //using joi to handle schema validation
+        // console.log(result);
+        // if(result.error){
+        //     throw new ExpressError(400, result.error);
+        // }
+        const newListing=new Listing(req.body.listing);          //req.body.listing returns a javascript object
+
+        //it is validation for each values but it is lengthy process so we use joi
+        // if(!newListing.title){
+        //     throw new ExpressError(400,"Title not defined");
+        // }
+        // if(!newListing.description){
+        //     throw new ExpressError(400,"Description not defined");
+        // }
+        // if(!newListing.location){
+        //     throw new ExpressError(400,"Location is empty");
+        // }
+        await newListing.save();
+        res.redirect("/listings");
+}));
+
+//edit route
+router.get("/:id/edit", wrapAsync(async (req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+}));
+
+//update route
+router.put("/:id",validateListing, wrapAsync(async (req,res)=>{
+    let {id}=req.params;
+    // if(!req.body.listing){
+    //     throw new ExpressError(400,"Invalid Data");
+    // }
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+}));
+
+//delete route
+router.delete("/:id",wrapAsync(async (req,res)=>{
+    let {id}=req.params;
+    let deletedListing= await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+}));
+
+// app.get("/testListing",async (req,res)=>{
+//     let sampleListing=new Listing({
+//         title:"My New Villa",
+//         description:"By The Beach",
+//         price:2300,
+//         location:"Noida, UP",
+//         country:"India"
+//     });
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful listing");
+// });
+
+module.exports=router;
